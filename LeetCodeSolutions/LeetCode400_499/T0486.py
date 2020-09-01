dc7125cd28b75990b1f6aa4c3e1ec24c84188c16
llall555 <3348509799@qq.com>
2020-09-01 09:21:03 +0800

    T486 Daily Solution 2020.9.1 None

---
+++ LeetCodeSolutions/LeetCode400_499/T0486.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Author  :   JingV
+@Version :   1.0
+@Contact :   None
+@License :   None
+@Desc    :   None
+'''
+
+
+class Solution(object):
+    def PredictTheWinner(self, nums):
+        pass
+
+
+def main():
+    solution = Solution()
+    tests = []
+    for nums in tests:
+        print(solution.PredictTheWinner(nums))
+
+
+if __name__ == "__main__":
+    main()
